f33bf4458e3c2289140e2f7efb8ad4afd53262c2
eashaakram <eashaakram63@gmail.com>
2025-07-03 15:13:45 +0500

    Comments

---
+++ Comment.py
@@ -0,0 +1,7 @@
+# Author: Easha Akram
+# Course: Python 
+# Title: Comment
+print("Today is my Second Day of doing Python Coding")#This is single line comment 
+'''This is multiple line comment and in this we will calculate year
+ in seconds by using the formula 365*24*60*60'''
+print("Year in second = ", 365*24*60*60)
