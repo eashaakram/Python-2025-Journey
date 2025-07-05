28c5f168763f026ab4e3887389265c721f5af099
eashaakram <eashaakram63@gmail.com>
2025-07-05 18:47:58 +0500

    variable

--- MainFolder.py/beginning.py
+++ MainFolder.py/beginning.py
@@ -1,4 +1,5 @@
 print("Greetings!!\n","Myself Easha Akram" )
 print("Today I'm Writing my first code in Python Language")
-print("Lets Start with Simple Calculation","\nWhat will be answer of 11 multiplay by 2345")
+print("Let's Start with Simple Calculation","\nWhat will be answer of 11 multiplay by 2345")
 print("Solve it by Python Code","\n11 * 2345 = ",11*2345)
+
---
+++ MainFolder.py/variable&data_type.py
@@ -0,0 +1,12 @@
+a = 1
+b = True
+c = "Python" 
+d = None
+print(a)
+print(b)
+print(c)
+print(d)
+print("The type of a is",type(a))
+print("The type of b is",type(b))
+print("The type of c is",type(c))
+print("The type of d is",type(d))
