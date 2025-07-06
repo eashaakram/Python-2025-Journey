5eea5c2710cfc4847cbb8c0ef3d18f096fae9812
eashaakram <eashaakram63@gmail.com>
2025-07-06 17:38:36 +0500

    calculator

--- MainFolder.py/Calculator.py
+++ MainFolder.py/Calculator.py
@@ -1,9 +1,9 @@
 # This Code is about Arithmetic Operators
 print("\t****Welcome to Calculator****\n")
-num1=int(input("Enter first number: ")) #Addition
-num2=int(input("Enter second number: "))
-print("Addition: ",num1,"+",num2,"=",num1+num2)
-print("Subtraction: ",num1,"-",num2,"=",num1-num2)
-print("Multiplication: ",num1,"*",num2,"=",num1*num2)
-print("Division: ",num1,"/",num2,"=",num1/num2)
-print("Modulus: ",num1,"%",num2,"=",num1%num2)
+num1=int(input("Enter first number: ")) 
+num2=int(input("Enter second number: ")) 
+print("Addition: ",num1,"+",num2,"=",num1+num2) #Addition
+print("Subtraction: ",num1,"-",num2,"=",num1-num2) #Subtraction
+print("Multiplication: ",num1,"*",num2,"=",num1*num2) #Multipliaction
+print("Division: ",num1,"/",num2,"=",num1/num2) #Division
+print("Modulus: ",num1,"%",num2,"=",num1%num2) #Modulus
