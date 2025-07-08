7ae16e70e0ce8c21efd4bcaa71d0041bc92b8fa2
eashaakram <eashaakram63@gmail.com>
2025-07-08 14:25:48 +0500

    typecasting

--- MainFolder.py/TypeCasting.py
+++ MainFolder.py/TypeCasting.py
@@ -6,3 +6,8 @@ print(a+b) #string
 print(int(a)+int(b)) #string to integer
 print(c+d) #integer
 
+string = "15"
+number = 7
+string_number = int(string) #give an error if the string is not valid integer
+sum = number + string_number
+print("The sum of number",number,"and",string_number,"is",sum)
