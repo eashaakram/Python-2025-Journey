de9a88d7144f97fb7949aed91dcb8c82a35bc286
eashaakram <eashaakram63@gmail.com>
2025-07-08 16:06:45 +0500

    how to take input

---
+++ MainFolder.py/Taking_input.py
@@ -0,0 +1,27 @@
+#Check how to give input
+#Take any Input as string
+a = input("Enter any number: ")
+print(a)
+# print("Enter any number: ")
+# a = input()
+# print(a)
+
+
+#Now using proper syntax for string
+#Take string input directly
+b = input("Enter your course name: ")
+
+#Then Take integer input and add
+c = int(input("Enter first number: "))
+d = int(input("Enter second number: "))
+print("Sum:",c+d)
+
+#and then take input as string convert it into int and give both results
+e = input("Enter first number: ")
+f = input("Enter second number: ")
+print("String:",e+f)
+#Arithmetic Operations
+print("Addition:",int(e)+int(f))
+print("Subtraction:",int(e)-int(f))
+print("Multiplication:",int(e)*int(f))
+print("Divison:",int(e)/int(f))
