1ff97eda8185de1ef7d7b37309231ee5bfb288f5
eashaakram <eashaakram63@gmail.com>
2025-07-08 15:25:11 +0500

    typecasting

--- MainFolder.py/TypeCasting.py
+++ MainFolder.py/TypeCasting.py
@@ -11,3 +11,18 @@ number = 7
 string_number = int(string) #give an error if the string is not valid integer
 sum = number + string_number
 print("The sum of number",number,"and",string_number,"is",sum)
+
+#Implicit Typecasting 
+#python automatically converts e to int 
+e = 7
+print("e =",e)
+print(type(e))
+#python automatically converts f to float
+f = 3.0
+print("f =",f)
+print(type(f))
+#python automatically converts g to float as it is a float addition
+g = e + f
+print("The Sum of",e,"and",f,"is",g)
+print(type(g))
+
