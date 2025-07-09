7240e0a81315539a9ef7339f7b12b6ef0b09a708
eashaakram <eashaakram63@gmail.com>
2025-07-09 22:16:34 +0500

    string

--- MainFolder.py/String.py
+++ MainFolder.py/String.py
@@ -1,5 +1,23 @@
 #Anything enclose in double or single quote is string
 name = "Python"
 print("This language is "+name)
+
 #Another way to write it
 print('Hello,"I am learning Python"')
+
+#Triple single/double quote
+print('''I'm Easha
+ And I'm doing SE''')
+
+#Accessing Character of string 
+name = "Easha"
+print(name[0]) #print first character of name Easha=E
+print(name[1]) #print second character of name Easha=a
+print(name[2]) #print second character of name Easha=s
+print(name[3]) #print second character of name Easha=h
+print(name[4]) #print second character of name Easha=a
+#index 5 give an error
+
+#for loop with string
+for character in name:
+ print(character)
