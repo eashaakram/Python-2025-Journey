57c8e90a3ae646a8a2bea12e2f8572b23f7cfb4d
eashaakram <eashaakram63@gmail.com>
2025-07-10 21:20:30 +0500

    string

--- MainFolder.py/String.py
+++ MainFolder.py/String.py
@@ -26,7 +26,7 @@ for character in name:
 course = "Python,language"
 print(len(course))
 print("To start from zero index")
-print(course[0:6]) 
+print(course[0:6]) #including zero but not 6
 print("To start from zero index without mention it")
 print(course[:6]) #if we skip 0 Python atomatically consider zero 
 print("To start from index 1")
@@ -35,3 +35,12 @@ print("If we don't mention any index then it print full word")
 print(course[:])
 print("If we mention negative in index then it will minus from total")
 print(course[:-3])
+print("One more way")
+print(course[2:len(course)-3])
+#Quick Quiz
+nm = "harry"
+print(nm[-4:-2])
+'''It will first minus 4 from total which is 5 so 
+index start from 1 and then it will minus 2 from 5 which left with 
+3 index so it will print from 1 to 3 index means 1,2 because in 
+that form[1:3] 1 is included and 3 is excluded'''
