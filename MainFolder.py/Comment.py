868df925b8c724018bca7c0d0dfc9247446d84bc
eashaakram <eashaakram63@gmail.com>
2025-07-13 20:33:23 +0500

    Comment

--- MainFolder.py/Comment.py
+++ MainFolder.py/Comment.py
@@ -5,5 +5,5 @@ print("Today is my Second Day of doing Python Coding")#This is single line comme
 '''This is multiple line comment and in this code now we will calculate year
  in seconds by using the formula 365*24*60*60'''
 print("Year in second =", 365*24*60*60)
-'''To write single line comment we use hashtag symbol {#} and for multiple lines of comment we can 
-use triple single/double quotes {"/'}'''
+print('''To write single line comment we use hashtag symbol {#} and for multiple lines of comment we can 
+use triple single/double quotes {"/'}''')
