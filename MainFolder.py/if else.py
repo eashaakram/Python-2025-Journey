bd281af818ba062f4edacc5ed5be0f1bcc2cd511
eashaakram <eashaakram63@gmail.com>
2025-07-14 17:59:30 +0500

    if else

--- MainFolder.py/if else.py
+++ MainFolder.py/if else.py
@@ -2,9 +2,12 @@
 a = int(input("Enter your age:"))
 print("Your age is",a)
 if(a>18):
-    print("You can drive")
+    print("You can drive") #indentation use in if-else to refer block of statement 
+    print("Yes")           #basically indentation is a space before print
 else:
     print("You can't drive")
+    print("No")
+print("Yay!")#this is not in block of statement
 #Conditional Operators
 # >,<,>=,<=,==,!=
 print(a<18)
