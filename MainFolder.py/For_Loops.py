857c194b278b11cc097cbcbc56351a29e2908e39
eashaakram <eashaakram63@gmail.com>
2025-07-16 18:05:36 +0500

    for loops

--- MainFolder.py/For_Loops.py
+++ MainFolder.py/For_Loops.py
@@ -6,3 +6,18 @@ for i in name:
         print("there is something special")
 for i in name:
     print(i, end=", ")
+print("\n")
+
+colors = ["Red", "Blue", "Green", "Yellow"]
+for color in colors:
+    print(color)
+    for i in color:
+        print(i)
+
+#range
+for k in range(5):
+    print(k)
+for k in range(5):
+    print(k+1)
+for k in range(1,10):
+    print(k)
