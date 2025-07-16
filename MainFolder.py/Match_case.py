1d3b8d887646e3348124be4ddb81b3140a685987
eashaakram <eashaakram63@gmail.com>
2025-07-16 17:32:31 +0500

    Match case

--- MainFolder.py/Match_case.py
+++ MainFolder.py/Match_case.py
@@ -5,5 +5,10 @@ match x:
         print("case is zero")
     case 4:
         print("case is four")
-    case _:
-        print(x)
+    case _ if x!=90:
+        print(x,"is not 90")
+    case _ if x!=80:
+        print(x,"is not 80")
+    case _ if x!=70:
+        print(x,"is not 70")
+    
