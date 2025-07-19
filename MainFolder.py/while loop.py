f4e56c89e34d61f959c2fb3345bf6b1232ab3b96
eashaakram <eashaakram63@gmail.com>
2025-07-19 21:52:06 +0500

    while loop

---
+++ MainFolder.py/while loop.py
@@ -0,0 +1,13 @@
+# i = int(input("Enter any number:"))
+# while(i < 30):
+#     i = int(input("Enter any number:"))
+#     print(i)
+# print("Done with the loop")
+
+
+i = int(input("Enter any number: "))
+while(i < 30):
+    print(i)
+    i += 1  # increment i to eventually break the loop
+print("Done with the loop")
+
