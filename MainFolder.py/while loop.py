9a6b4dbd3e3b3829f5308785576e051777ef8948
eashaakram <eashaakram63@gmail.com>
2025-07-20 15:27:53 +0500

    while loop

--- MainFolder.py/while loop.py
+++ MainFolder.py/while loop.py
@@ -1,13 +1,28 @@
-# i = int(input("Enter any number:"))
-# while(i < 30):
-#     i = int(input("Enter any number:"))
-#     print(i)
-# print("Done with the loop")
+i = int(input("Enter any number:"))
+while(i < 30):
+    i = int(input("Enter any number:"))
+    print(i)
+print("Done with the loop")
 
 
 i = int(input("Enter any number: "))
 while(i < 30):
-    print(i)
+    print("i =",i)
     i += 1  # increment i to eventually break the loop
 print("Done with the loop")
 
+#Decrement Loop
+count = 5
+while(count > 0):
+    print("Count =",count)
+    count = count - 1 
+    
+ #else    
+print("\nWith Else ")
+t = -5
+while(t > 0):
+    print("t =",t)
+    t = t - 1 
+    #When while loop false it then print else part
+else:
+    print("I'm inside the else part")
