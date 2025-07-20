14f949830d11c52205fe5e822bacd51a590fe9c6
eashaakram <eashaakram63@gmail.com>
2025-07-20 16:00:24 +0500

    for loop

--- MainFolder.py/For_Loops.py
+++ MainFolder.py/For_Loops.py
@@ -15,9 +15,12 @@
 #         print(i)
 
 #range
+print("K values above 5")
 for k in range(5): #n-1
     print(k)
+print("K range with 5")
 for k in range(5):
     print(k+1)
+print("K range from 1 to 10")    
 for k in range(1,11):
     print(k)
