11bb7659da71d876074a5bbd741ccd8a9d50a49c
eashaakram <eashaakram63@gmail.com>
2025-07-21 15:07:19 +0500

    break and continue

--- MainFolder.py/break and continue.py
+++ MainFolder.py/break and continue.py
@@ -1,12 +1,19 @@
-#break
-for i in range(10):
-    if (i == 5):
-      break #it will stop here
-    print("5 *",i+1,"=",5*(i+1))
+# #break
+# for i in range(10):
+#     if (i == 5):
+#       break #it will stop here
+#     print("5 *",i+1,"=",5*(i+1))
 
-#continue
-for i in range(10):
-    if (i == 5):
-      print("skip")
-      continue #it will just skip the iteration at 6
-    print("2 *",i+1,"=",2*(i+1))
+# #continue
+# for i in range(10):
+#     if (i == 5):
+#       print("skip")
+#       continue #it will just skip the iteration at 6
+#     print("2 *",i+1,"=",2*(i+1))
+
+f = 0
+while True:
+   print(f)
+   f+=1
+   if(f%5==0):
+    break
