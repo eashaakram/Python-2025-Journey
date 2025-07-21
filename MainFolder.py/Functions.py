7ae46d6ccfc0535b106f92e1de52976adec9181c
eashaakram <eashaakram63@gmail.com>
2025-07-21 15:51:07 +0500

    functions

---
+++ MainFolder.py/Functions.py
@@ -0,0 +1,10 @@
+#Functions
+#Built in functions start with def
+def calculateGmean(a, b):  #function body
+    mean = (a*b)/(a+b)
+    print("Mean =",mean)
+
+a = 2
+b = 3
+calculateGmean(a, b) #calling a function 
+
