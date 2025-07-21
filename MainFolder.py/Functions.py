db8557bd3c93c0d699ff903530ada21f52e1e495
eashaakram <eashaakram63@gmail.com>
2025-07-21 15:58:25 +0500

    Functions

--- MainFolder.py/Functions.py
+++ MainFolder.py/Functions.py
@@ -3,8 +3,19 @@
 def calculateGmean(a, b):  #function body
     mean = (a*b)/(a+b)
     print("Mean =",mean)
+def isGreater(a , b):
+    if (a>b):
+        print("First number is greater")
+    else:
+        print("Second number is greater")
+def islesser(a , b):
+    pass #define it later so interpreter do not generate error
 
 a = 2
 b = 3
 calculateGmean(a, b) #calling a function 
+isGreater(a , b)
+islesser(a, b)
+
+
 
