f12e7e1ced757302176a8c67d2c82d717f5ada5e
eashaakram <eashaakram63@gmail.com>
2025-07-23 23:02:29 +0500

    arguments

--- MainFolder.py/Function argument.py
+++ MainFolder.py/Function argument.py
@@ -13,3 +13,15 @@ def averageee(e = 9, f = 1):  #default argument here give value to c and d
     print("3)The average is",(e+f)/2)
 
 averageee(1,5) #but if we write here values then default argument will get ignored
+
+
+def avg(g = 9,h = 1):        
+    print("4)The average is",(g+h)/2)
+#keyword argument (order doesn't matter)
+avg(h = 9,g = 21)
+ 
+ 
+def ave(i,j = 1):        
+    print("1)The average is",(i+j)/2)
+
+ave(i = 21) #required argument, the value of i is complusory 
