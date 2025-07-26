6c16f2507216f90ff149ec90877e8d217bc875b2
eashaakram <eashaakram63@gmail.com>
2025-07-27 02:07:44 +0500

    list

---
+++ MainFolder.py/List.py
@@ -0,0 +1,13 @@
+marks = [3, 5 , 6, "Eesha", True]
+print(marks) #this will print whole list elements
+print(marks[0]) #it will print 3 number at index zero
+print(marks[1]) 
+print(marks[2])
+print(marks[3])
+print(marks[4])
+
+lst1 = [1, 2, 2, 3, 5, 4, 6]
+lst2 = ["Red", "Green", "Blue"]
+print(lst1)
+print(lst2)
+
