0157db84417a79ee7fcd6a947c52c166d96b1371
eashaakram <eashaakram63@gmail.com>
2025-07-30 17:05:02 +0500

    tuple

---
+++ MainFolder.py/Tuple.py
@@ -0,0 +1,8 @@
+tup = (1,5,3)
+print(type(tup),tup)
+tup = (1) #it will print int type bcz we don't put coma at its end
+print(type(tup),tup)
+tup = (1,) #in tuple coma is necessay
+print(type(tup),tup)
+
+
