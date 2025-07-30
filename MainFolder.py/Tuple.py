c50563d37b37a86e5bcf32d7c99dfb2d6d892f5a
eashaakram <eashaakram63@gmail.com>
2025-07-30 17:18:35 +0500

    tuple

--- MainFolder.py/Tuple.py
+++ MainFolder.py/Tuple.py
@@ -5,4 +5,27 @@ print(type(tup),tup)
 tup = (1,) #in tuple coma is necessay
 print(type(tup),tup)
 
+# tup = (1,2,16,343,98)
+# tup[0] = 90 
+# it will generate error because we can't change in tuple
 
+tup = [1,2,16,343,98] #it is list not tuple
+tup[0] = 90 
+print(tup)
+
+tup = (1,2,76)
+if 76 in tup:
+    print("Yes 76 is present")
+
+'''Tuples are imutable if you want to change tuple 
+then first convert it into list, make changes in it
+and then again convert back it into tuple'''
+
+countries = ("Spain", "Italy", "Pakistan", "England", "Germany")
+print("Countries before change:",countries)
+temp = list(countries)
+temp.append("Russia") #Add item
+temp.pop(3) #Remove item
+temp[2] = "Finland" #Change item
+countries = tuple(temp)
+print("Countries after change:",countries)
