800ebf704c864d12a106ce2223552bc33180bfc8
eashaakram <eashaakram63@gmail.com>
2025-08-05 15:08:07 +0500

    loops

--- MainFolder.py/while loop.py
+++ MainFolder.py/while loop.py
@@ -1,28 +1,39 @@
-i = int(input("Enter any number:"))
-while(i < 30):
-    i = int(input("Enter any number:"))
-    print(i)
-print("Done with the loop")
+# i = int(input("Enter any number:"))
+# while(i < 30):
+#     i = int(input("Enter any number:"))
+#     print(i)
+# print("Done with the loop")
 
 
-i = int(input("Enter any number: "))
-while(i < 30):
-    print("i =",i)
-    i += 1  # increment i to eventually break the loop
-print("Done with the loop")
+# i = int(input("Enter any number: "))
+# while(i < 30):
+#     print("i =",i)
+#     i += 1  # increment i to eventually break the loop
+# print("Done with the loop")
 
-#Decrement Loop
-count = 5
-while(count > 0):
-    print("Count =",count)
-    count = count - 1 
+# #Decrement Loop
+# count = 5
+# while(count > 0):
+#     print("Count =",count)
+#     count = count - 1 
     
- #else    
-print("\nWith Else ")
-t = -5
-while(t > 0):
-    print("t =",t)
-    t = t - 1 
-    #When while loop false it then print else part
-else:
-    print("I'm inside the else part")
+#  #else    
+# print("\nWith Else ")
+# t = -5
+# while(t > 0):
+#     print("t =",t)
+#     t = t - 1 
+#     #When while loop false it then print else part
+# else:
+#     print("I'm inside the else part")
+
+
+count = 1
+while (count <= 5):
+    count += 1
+print(count)
+
+i = 1
+while(i <= 100):
+    print(i)
+    i+=1
