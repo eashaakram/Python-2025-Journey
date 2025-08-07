136225bb1978138afc046ed907d80b26237d0537
eashaakram <eashaakram63@gmail.com>
2025-08-07 16:28:47 +0500

    loops

--- MainFolder.py/For_Loops.py
+++ MainFolder.py/For_Loops.py
@@ -1,31 +1,35 @@
-# name = "Python"
-# for i in name:
-#     print(i)
-#     # print(i, end=", ")
-#     if(i=="h"):
-#         print("there is something special")
-# for i in name:
-#     print(i, end=", ")
-# print("\n")
+name = "Python"
+for i in name:
+    print(i)
+    # print(i, end=", ")
+    if(i=="h"):
+        print("there is something special")
+for i in name:
+    print(i, end=", ")
+print("\n")
 
-# colors = ["Red", "Blue", "Green", "Yellow"]
-# for color in colors:
-#     print(color)
-#     for i in color:
-#         print(i)
+colors = ["Red", "Blue", "Green", "Yellow"]
+for color in colors:
+    print(color)
+    for i in color:
+        print(i)
 
-# #range
-# print("K values above 5")
-# for k in range(5): #n-1
-#     print(k)
-# print("K range with 5")
-# for k in range(5):
-#     print(k+1)
-# print("K range from 1 to 10")    
-# for k in range(1,11):
-#     print(k)
+#range
+print("K values above 5")
+for k in range(5): #n-1
+    print(k)
+print("K range with 5")
+for k in range(5):
+    print(k+1)
+print("K range from 1 to 10")    
+for k in range(1,11):
+    print(k)
 
 a = "Akram"
 for i in range(0,10):
     print(f"{i+1}) Easha {a}")
 
+#Table
+num = int(input("Enter number:"))
+for i in range(1,11):
+    print(f"{num} x {i} = {num*i}")
