37411db54f770a979e17d88acbcb4d364b897a50
eashaakram <eashaakram63@gmail.com>
2025-08-12 23:46:06 +0500

    functions

--- MainFolder.py/Functions.py
+++ MainFolder.py/Functions.py
@@ -1,21 +1,24 @@
-#Functions
-#Built in functions start with def
-def calculateGmean(a, b):  #function body
-    mean = (a*b)/(a+b)
-    print("Mean =",mean)
-def isGreater(a , b):
-    if (a>b):
-        print("First number is greater")
-    else:
-        print("Second number is greater")
-def islesser(a , b):
-    pass #define it later so interpreter do not generate error
+# #Functions
+# #Built in functions start with def
+# def calculateGmean(a, b):  #function body
+#     mean = (a*b)/(a+b)
+#     print("Mean =",mean)
+# def isGreater(a , b):
+#     if (a>b):
+#         print("First number is greater")
+#     else:
+#         print("Second number is greater")
+# def islesser(a , b):
+#     pass #define it later so interpreter do not generate error
 
-a = 2
-b = 3
-calculateGmean(a, b) #calling a function 
-isGreater(a , b)
-
-
+# a = 2
+# b = 3
+# calculateGmean(a, b) #calling a function 
+# isGreater(a , b)
 
 
+
+
+list = ['h','i','h','o','w','y','o','u','!','!']
+print("The count of i is: ",list.count('i'))
+print("The count of o is: ",list.count('o'))
