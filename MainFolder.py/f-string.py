f42896df2458f33e38fd1aaa2e5793e0c00d24aa
eashaakram <eashaakram63@gmail.com>
2025-08-17 15:11:34 +0500

    f-string

--- MainFolder.py/f-string.py
+++ MainFolder.py/f-string.py
@@ -8,5 +8,6 @@ print(f"Hey my name is {name} and I'm from {country}")
 price = 49.0399
 txt = f"For only {price:.2f} dollars!"
 print(txt.format(price = 49.0399))
+print(f"{2*30}")
 #to know any type of details about syntax
 help("format")
