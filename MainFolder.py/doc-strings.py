f3e1b190fb0c553f2f74e3cd276ea700e5000548
eashaakram <eashaakram63@gmail.com>
2025-08-17 16:04:52 +0500

    doc-string

---
+++ MainFolder.py/doc-strings.py
@@ -0,0 +1,20 @@
+#doc-strings
+#doc-strings write just after or above the function defination
+def square(n):
+    '''Takes in a number n, returns the 
+    square of n'''
+    print(n**2)
+
+square(5)
+print(square.__doc__)
+ 
+#if we don't write it after or above function defination then it will not doc-string
+#like that
+def square(n):
+    print(n)
+    '''Takes in a number n, returns the 
+    square of n'''
+    print(n**2)
+
+square(5)
+print(square.__doc__)#it will give none as output
