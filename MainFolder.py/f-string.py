8c3aa8b27bbe5dba4b4f8846c32d76c2e723165f
eashaakram <eashaakram63@gmail.com>
2025-08-17 15:10:02 +0500

    f-string

---
+++ MainFolder.py/f-string.py
@@ -0,0 +1,12 @@
+#f-string
+letter = "Hey my name is {0} ans I am from {1}"
+name = "Easha"
+country = "Pakistan"
+print(letter.format(name , country))
+print(f"Hey my name is {name} and I'm from {country}")
+
+price = 49.0399
+txt = f"For only {price:.2f} dollars!"
+print(txt.format(price = 49.0399))
+#to know any type of details about syntax
+help("format")
