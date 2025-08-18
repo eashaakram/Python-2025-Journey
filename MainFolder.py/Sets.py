a4ea2df6546d5e02b6e40dfbdc640a77069ad38c
eashaakram <eashaakram63@gmail.com>
2025-08-18 16:13:08 +0500

    sets

--- MainFolder.py/Sets.py
+++ MainFolder.py/Sets.py
@@ -1,5 +1,12 @@
-#It is not set it will become dectionary 
-# harry = {}
-#for printing its type as a set we will write it like that
-harry = set()
-print(type(harry))
+# #It is not set it will become dectionary 
+# # harry = {}
+# #for printing its type as a set we will write it like that
+# harry = set()
+# print(type(harry))
+
+info = {"Carla", 19, False, 5.9, 19} #Set will remove duplicates
+print(info)
+#Set is unordered
+for value in info:
+    print(value,end=" ")
+
