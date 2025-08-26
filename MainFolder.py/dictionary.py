7956b036ba5ae45b801cdcd28f2be238a433f1c0
eashaakram <eashaakram63@gmail.com>
2025-08-27 01:51:30 +0500

    dictionary

--- MainFolder.py/dictionary.py
+++ MainFolder.py/dictionary.py
@@ -1,34 +1,34 @@
-# #dictionaries
-# dic = {"Easha" : "Human being",
-#        "Spoon" : "Object"}
-# print(dic["Easha"])
+#dictionaries
+dic = {"Easha" : "Human being",
+       "Spoon" : "Object"}
+print(dic["Easha"])
 
-# info = {"Name" : "Easha",
-#         "Age" : 19,
-#         "Eligible" : True}
-# print(info) 
+info = {"Name" : "Easha",
+        "Age" : 19,
+        "Eligible" : True}
+print(info) 
 
-# #accessing keys
-# print(info.keys()) 
+#accessing keys
+print(info.keys()) 
 
-# # accessing values
-# print(info.values())
+# accessing values
+print(info.values())
 
-# # for key in info.keys(): 
-# #     print(info[key])
+# for key in info.keys(): 
+#     print(info[key])
 
-# # for key in info.keys():
-# #     print(f"The value of corresponding to key {key} is {info[key]}")
+# for key in info.keys():
+#     print(f"The value of corresponding to key {key} is {info[key]}")
 
-# for key,value in info.items():
-#         print(f"The value of corresponding to key {key} is {value}")
+for key,value in info.items():
+        print(f"The value of corresponding to key {key} is {value}")
 
-# #Two ways of getting any single key
-# print(info["Name"])#If I wrote Name2 it will generate error
-# print(info.get("Name"))#If I wrote Name2 it will not generate error
-# print(info.get("Name2"))#it will give output none
+#Two ways of getting any single key
+print(info["Name"])#If I wrote Name2 it will generate error
+print(info.get("Name"))#If I wrote Name2 it will not generate error
+print(info.get("Name2"))#it will give output none
 
-# print(info.items())
+print(info.items())
  
 
 #Dictionary Methods
