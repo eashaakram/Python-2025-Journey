63e19a01c2df379d22923a1f110857cbcc77bef2
eashaakram <eashaakram63@gmail.com>
2025-08-27 01:50:17 +0500

    var&dt

--- MainFolder.py/variable&data_type.py
+++ MainFolder.py/variable&data_type.py
@@ -12,3 +12,13 @@ print("The type of a is",type(a))
 print("The type of b is",type(b))
 print("The type of c is",type(c))
 print("The type of d is",type(d))
+
+name = "Easha"
+age = 19
+city = "grw"
+print("My name is",name,", My age is",age,"and I'm from",city)
+
+com = 9+7j
+print(com,type(com))
+print("Real part is",com.real)
+print("Imaginary part is",com.imag)
