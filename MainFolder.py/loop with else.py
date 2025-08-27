fd4605403a976715f97e07a2c15fbea5ccc42a28
eashaakram <eashaakram63@gmail.com>
2025-08-27 16:58:34 +0500

    loop with else

--- MainFolder.py/loop with else.py
+++ MainFolder.py/loop with else.py
@@ -4,3 +4,20 @@ for i in range(5):
 else:
     print("Sorry no i")
 
+#while loop with else
+i = 0
+while i<7:
+    print(i)
+    i=i+1
+    if(i==4):
+        print("Here the loop break")
+        break
+else:
+    print("Sorry no i")
+
+#format method
+for x in range(5):
+    print("Iteration number {} in for loop".format(x+1))
+else:
+    print("Else block i loop")
+print("Out of loop")
