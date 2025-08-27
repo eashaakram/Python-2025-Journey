1c0ec18e6e5356c2d9b9dc788b6d9190e624166c
eashaakram <eashaakram63@gmail.com>
2025-08-28 01:04:17 +0500

    typecasting

--- MainFolder.py/TypeCasting.py
+++ MainFolder.py/TypeCasting.py
@@ -1,28 +1,32 @@
-a = "1"
-b = "2"
-c = 1
-d = 2
-print(a+b) #string 
-print(int(a)+int(b)) #string to integer
-print(c+d) #integer
+# a = "1"
+# b = "2"
+# c = 1
+# d = 2
+# print(a+b) #string 
+# print(int(a)+int(b)) #string to integer
+# print(c+d) #integer
 
-string = "15"
-number = 7
-string_number = int(string) #give an error if the string is not valid integer
-sum = number + string_number
-print("The sum of number",number,"and",string_number,"is",sum)
+# string = "15"
+# number = 7
+# string_number = int(string) #give an error if the string is not valid integer
+# sum = number + string_number
+# print("The sum of number",number,"and",string_number,"is",sum)
 
-#Implicit Typecasting 
-#python automatically converts e to int 
-e = 7
-print("e =",e)
-print(type(e))
-#python automatically converts f to float
-f = 3.0
-print("f =",f)
-print(type(f))
-#python automatically converts g to float as it is a float addition
-g = e + f
-print("The Sum of",e,"and",f,"is",g)
-print(type(g))
+# #Implicit Typecasting 
+# #python automatically converts e to int 
+# e = 7
+# print("e =",e)
+# print(type(e))
+# #python automatically converts f to float
+# f = 3.0
+# print("f =",f)
+# print(type(f))
+# #python automatically converts g to float as it is a float addition
+# g = e + f
+# print("The Sum of",e,"and",f,"is",g)
+# print(type(g))
 
+num1="14"
+num2=34
+new=int(num1)
+print(new+num2)
