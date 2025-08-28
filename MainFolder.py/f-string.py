957c11dd006e37f1634be30c7fc3b3d51e814670
eashaakram <eashaakram63@gmail.com>
2025-08-29 01:24:26 +0500

    f-string

--- MainFolder.py/f-string.py
+++ MainFolder.py/f-string.py
@@ -11,3 +11,10 @@ print(txt.format(price = 49.0399))
 print(f"{2*30}")
 #to know any type of details about syntax
 help("format")
+
+#example
+item_name = input("Enter name of the item: ")
+price = float(input("Enter price of item: "))
+quantity = int(input("Enter quantity of price: "))
+cost = price*quantity
+print(f"You bought {quantity} {item_name} at Rs.{price} each \nYour total bill is Rs.{cost}.")
