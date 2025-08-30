33311b47f2447d32ce6964884da4cbcfc00dcca0
eashaakram <eashaakram63@gmail.com>
2025-08-30 16:22:40 +0500

    ass

---
+++ MainFolder.py/Practice.py/assignment1.py
@@ -0,0 +1,12 @@
+#Practice
+#Problem 1: Take a number, add 10, multiply by 2, then subtract 5 (use assignment operators).
+#Take input
+number = int(input("Enter any number: "))
+#Addition
+add = number + 10
+#Multipliaction
+multiply = add * 2
+#Subtract
+subtract = multiply - 5 
+#Output
+print("Result =",subtract)
