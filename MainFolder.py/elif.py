e040c55f1e43502b4286fb81725b85babc5a22fc
eashaakram <eashaakram63@gmail.com>
2025-09-01 22:33:33 +0500

    if else

--- MainFolder.py/elif.py
+++ MainFolder.py/elif.py
@@ -1,11 +1,11 @@
-# #elif
-# num = int(input("Enter a number:"))
-# if(num<0):
-#     print("Number is negative")
-# elif(num>0):
-#     print("Number is positive")
-# else:
-#     print("Number is zero")
+#elif
+num = int(input("Enter a number:"))
+if(num<0):
+    print("Number is negative")
+elif(num>0):
+    print("Number is positive")
+else:
+    print("Number is zero")
 
 marks = int(input("Enter your marks between(0 to 100): "))
 if marks < 0 or marks > 100:
--- MainFolder.py/if else.py
+++ MainFolder.py/if else.py
@@ -1,29 +1,29 @@
-# #If else statement 
-# a = int(input("Enter your age:"))
-# print("Your age is",a)
-# if(a>18):
-#     print("You can drive") #indentation use in if-else to refer block of statement 
-#     print("Yes")           #basically indentation is a space before print
-# else:
-#     print("You can't drive")
-#     print("No")
-# print("Yay!")#this is not in block of statement
-# #Conditional Operators
-# # >,<,>=,<=,==,!=
-# print(a<18)
-# print(a>18)
-# print(a<=18)
-# print(a>=18)
-# print(a==18)
-# print(a!=18)
+#If else statement 
+a = int(input("Enter your age:"))
+print("Your age is",a)
+if(a>18):
+    print("You can drive") #indentation use in if-else to refer block of statement 
+    print("Yes")           #basically indentation is a space before print
+else:
+    print("You can't drive")
+    print("No")
+print("Yay!")#this is not in block of statement
+#Conditional Operators
+# >,<,>=,<=,==,!=
+print(a<18)
+print(a>18)
+print(a<=18)
+print(a>=18)
+print(a==18)
+print(a!=18)
 
-# #example
-# apple = 120
-# budget = 200
-# if(apple <= budget):
-#     print("Alexa, add 1kg Apples to the cart")
-# else:
-#     print("Alexa, add 1kg Apples to the cart")
+#example
+apple = 120
+budget = 200
+if(apple <= budget):
+    print("Alexa, add 1kg Apples to the cart")
+else:
+    print("Alexa, add 1kg Apples to the cart")
 
 #some practice
 a = "20"
