d355124bcdd7e572f43c57218525146af329cd0d
eashaakram <eashaakram63@gmail.com>
2025-09-08 00:12:07 +0500

    ass2

--- MainFolder.py/Practice.py/assignment2.py
+++ MainFolder.py/Practice.py/assignment2.py
@@ -4,9 +4,9 @@ num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 num3 = int(input("Enter third number: "))
 #check which number is smaller
-if num1 < num2 and num1 < num3:
+if num1 <= num2 and num1 <= num3:
   print(f"First number {num1} is smallest number")
-elif num2 < num1 and num2 < num3:
+elif num2 <= num1 and num2 <= num3:
   print(f"Second number {num2} is smallest number")
 else:
   print(f"Third number {num3} is smallest number")
