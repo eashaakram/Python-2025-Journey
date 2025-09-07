2924564b94a9a2cc956503f3c9e6e9bbae57ddd4
eashaakram <eashaakram63@gmail.com>
2025-09-07 20:04:57 +0500

    check num

--- MainFolder.py/Practice.py/check number.py
+++ MainFolder.py/Practice.py/check number.py
@@ -27,3 +27,27 @@ while True:
       break
     else:
        print("Invalid!Enter number like (1,2,3..)")
+
+'''Problem 2: Take a number n from the user as input and display the even numbers from 1 to n. Use continue in while loop. 
+Check if the number n is less then or equal to 1 display error.
+'''
+   #Take input
+num = int(input("Enter a number: "))
+#Check num < or = to 1
+if num <= 1:
+  print("Error: Number less or equal to 1\nEnter greater than 1")
+else:
+#display even number
+  print("Even numbers are:")
+  i = 1
+  while (i <= num):
+    if i % 2 != 0:
+      i += 1
+      continue
+    else:
+      print(i,end=" ")
+      i += 1
+
+'''Problem 3: Take a number n from the user as input and
+ print the sum of even numbers and sum of odd numbers from 1 to n.
+'''
