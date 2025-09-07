793156416152b6c69e431e3ed88cc86d34aafc23
eashaakram <eashaakram63@gmail.com>
2025-09-07 19:58:24 +0500

    guess a number.py

--- MainFolder.py/Practice.py/guess a number.py
+++ MainFolder.py/Practice.py/guess a number.py
@@ -9,3 +9,15 @@ elif guess < secret:
     print("Too low! Try a bigger number.")
 else:    
     print("Too high! Try a smaller number.")
+
+#continue
+#Guess a number game
+print(">>> Guess Number <<<")
+number = 1234
+while True:
+  guess = int(input("Enter number: "))
+  if guess == number:
+    print("You Win!")
+    break  
+  else:
+    print("Try Again")
