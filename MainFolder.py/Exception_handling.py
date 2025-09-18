aa416b9620abd061397a86b26035df8d3334e204
eashaakram <eashaakram63@gmail.com>
2025-09-18 21:42:30 +0500

    exception handling

--- MainFolder.py/Exception_handling.py
+++ MainFolder.py/Exception_handling.py
@@ -54,3 +54,11 @@ x = func1()
 print(x)
 
 #Now we are writing code that will generate error by programmer if user enter wrong input
+#Raising custom error
+a = int(input("5)Enter any value between 5 and 9: "))
+if (a < 5 or a > 9):
+    raise ValueError("Value should be between 5 and 9")
+
+salary = int(input("Enter your salary amount: "))
+if not 2000 < salary < 5000:
+    raise ValueError("Not a valid salary")
