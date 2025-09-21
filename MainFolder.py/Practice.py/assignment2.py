30eb9cd82e982980936afaa98adc6cfe5bd7b6ee
eashaakram <eashaakram63@gmail.com>
2025-09-21 20:33:11 +0500

    ass2

--- MainFolder.py/Practice.py/assignment2.py
+++ MainFolder.py/Practice.py/assignment2.py
@@ -109,3 +109,40 @@ else: #if negative
     print(f"{num}: Odd")
     num *= 2
     print(f"Updated Value: {num}")
+
+
+
+import string
+import random
+
+def encode(normal_lang):
+    '''Converts your code to secret code'''
+    if (len(normal_lang)<3):
+        print(normal_lang[::-1])
+    elif (len(normal_lang)>3):
+        letter = normal_lang[1:]+normal_lang[0]
+        word_end = "".join(random.choices(string.ascii_lowercase, k=3))
+        word_begin = "".join(random.choices(string.ascii_lowercase, k=3))
+        secret_code = word_begin + letter + word_end
+        print(f"Your secret code is: {secret_code}")
+    
+def decode(normal_lang):
+    '''Converts your secret code to code'''
+    if (len(normal_lang)<3):
+        print(normal_lang[::-1])
+    elif (len(normal_lang)>3):
+        word = normal_lang[3:-3]
+        letter = word[-1]+word[:-1]
+        print(f"Your code is {letter}")
+
+print("Hey! Do you want to encode or decode?")
+choice = int(input("1 for encode and 2 for decode: "))
+
+match choice:
+    case 1:
+        normal_lang = input("Enter you word to convert: ")
+        encode(normal_lang)
+    case 2:
+        normal_lang = input("Enter you word to convert: ")
+        decode(normal_lang)
+
